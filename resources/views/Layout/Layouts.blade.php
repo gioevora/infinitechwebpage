f3a0f917520973ceeadfa0fc793f98fd3c8f32f5
LycaMay <infinitech.lycanobleza@gmail.com>
2024-08-29 10:18:51 +0800

    update

--- resources/views/Layout/Layouts.blade.php
+++ resources/views/Layout/Layouts.blade.php
@@ -24,6 +24,11 @@
 
     @section('scripts')
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+        <script>
+            const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+            const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+        </script>
     @show
 
 </body>
